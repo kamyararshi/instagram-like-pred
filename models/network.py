34ea6a7193672953958d1be4ecdef50a3a1312be
Smmehdihosseini <smmehdihosseini@gmail.com>
2023-07-17 15:17:08 +0200

    Add Dropout 0.5 + Increase Feature Net Depth to 64

--- models/network.py
+++ models/network.py
@@ -3,86 +3,82 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
+
 class LikeCategoryPredictor(nn.Module):
-    def __init__(self, in_dim, image_in_dim, out_classes, convolutional=False, resnet_weights=torchvision.models.ResNet18_Weights.DEFAULT):
+    def __init__(self, in_dim, image_in_dim, out_classes, convolutional=False,
+                 resnet_weights=torchvision.models.resnet18(pretrained=True).state_dict()):
         super().__init__()
 
         if convolutional:
             self.feature = nn.Sequential(
-                nn.Conv1d(in_channels=in_dim, out_channels=256, kernel_size=1),
+                nn.Conv1d(in_channels=in_dim, out_channels=512, kernel_size=1),
+                nn.BatchNorm1d(512),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Conv1d(in_channels=512, out_channels=256, kernel_size=1),
                 nn.BatchNorm1d(256),
                 nn.ReLU(),
+                nn.Dropout(0.5),
                 nn.Conv1d(in_channels=256, out_channels=128, kernel_size=1),
                 nn.BatchNorm1d(128),
                 nn.ReLU(),
-                nn.Conv1d(in_channels=128, out_channels=64, kernel_size=1),
-                nn.BatchNorm1d(64),
-                nn.ReLU(),
-                nn.Conv1d(in_channels=64, out_channels=32, kernel_size=1),
-                nn.BatchNorm1d(32),
-                nn.ReLU(),
+                nn.Dropout(0.5),
             )
         else:
             self.feature = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=in_dim, out_features=256),
-            nn.BatchNorm1d(256),
-            nn.ReLU(),
-            nn.Linear(in_features=256, out_features=128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Linear(in_features=128, out_features=64),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Linear(in_features=64, out_features=32),
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
-            )  
+                nn.Flatten(),
+                nn.Linear(in_features=in_dim, out_features=512),
+                nn.BatchNorm1d(512),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(in_features=512, out_features=256),
+                nn.BatchNorm1d(256),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(in_features=256, out_features=128),
+                nn.BatchNorm1d(128),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(in_features=128, out_features=64),
+                nn.BatchNorm1d(64),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+            )
 
-        
-        # Output shape is 512
-        self.image_embedder = ImageEmbedder(in_dim=image_in_dim, weights=resnet_weights)
-        #TODO: Change the linear layer depending on the shape of the concatenated tensor
+        self.image_embedder = ImageEmbedder(in_dim=image_in_dim, out_dim=128, weights=resnet_weights)
+
         self.linear = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(512+32, 256),
+            nn.Linear(64 + 128, 256),
+            nn.Dropout(0.5),
             nn.Linear(256, 128),
+            nn.Dropout(0.5),
             nn.Linear(128, 64),
+            nn.Dropout(0.5),
             nn.Linear(64, out_features=out_classes),
-            #nn.Softmax(10)
         )
 
-
     def forward(self, x):
-        data, image = x[0], x[1] # Data, Image
+        data, image = x[0], x[1]  # Data, Image
         self.image_embedder.eval()
-        
+
         # Data feature extraction
         data_features = self.feature(data)
         image_embeddings = self.image_embedder(image)
-        # Concatentaion
-        concatenated = torch.concatenate([data_features, image_embeddings], dim=1)
-        # prediciton
+        # Concatenation
+        concatenated = torch.cat([data_features, image_embeddings], dim=1)
+        # Prediction
         out = self.linear(concatenated)
 
         return out
-    
-# TODO: Later we do regression on the like amount
-class Predictor(nn.Module):
-    def __init__(self, in_dim) -> None:
-        super().__init__()
-        None
 
-    def forward(self, x):
-        out = x
-        return out
-    
+
 class ImageEmbedder(nn.Module):
-    def __init__(self, in_dim, weights=None) -> None:
+    def __init__(self, in_dim, out_dim, weights=None):
         super().__init__()
         if weights is not None:
-            self.model = torchvision.models.resnet18(weights)
-
+            self.model = torchvision.models.resnet18(pretrained=False)
+            self.model.load_state_dict(weights)
         else:
             self.model = torchvision.models.resnet18()
 
@@ -90,10 +86,14 @@ class ImageEmbedder(nn.Module):
         for parameter in self.model.parameters():
             parameter.requires_grad = False
 
-        # Remove the last fc layer
-        self.model.fc = nn.Identity()
+        # Replace the last fc layer
+        self.model.fc = nn.Sequential(
+            nn.Linear(in_features=512, out_features=out_dim),
+            nn.BatchNorm1d(out_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+        )
 
     def forward(self, input):
         output = self.model(input)
         return output
-    
